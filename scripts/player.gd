675d054e2d18b04df405ce9bb2275ff073c921b0
Allyson Jeronimo <allysonjeronimo@gmail.com>
2025-10-02 20:37:27 -0300

    Aula 02/10/2025

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,36 @@
 extends Area2D
 
+signal hit
+
 var screen_size
 var speed = 400
 var velocity = Vector2.ZERO
 
 func _init():
 	pass
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.enabled = true
 	
+# _ready() função de inicialização, executa depois de todos
+# os nodes da cena iniciarem	
 func _ready():
+	# obtem tamanho da tela
 	screen_size = get_viewport_rect().size
 	
+# Chamado em loop 
 func _process(delta):
 	update_movement(delta)
 	update_animations()
 	
 func update_movement(delta):
+	# inicialmente a velocity é zero
 	velocity = Vector2.ZERO
 	
+	# se pressionar para direita, esquerda, 
+	# cima ou baixo, muda a velocity
 	if Input.is_action_pressed("move_left"):
 		velocity.x = -1
 	if Input.is_action_pressed("move_right"):
@@ -26,7 +40,9 @@ func update_movement(delta):
 	if Input.is_action_pressed("move_down"):
 		velocity.y = 1
 		
+	# incrementa posição baseado na multiplicação da velocity, speed e delta
 	position += velocity * speed * delta
+	# mantem player dentro do limite da tela
 	position = position.clamp(Vector2.ZERO, screen_size)
 		
 func update_animations():
@@ -41,3 +57,12 @@ func update_animations():
 			$AnimatedSprite2D.flip_v = velocity.y > 0
 	else:
 		$AnimatedSprite2D.stop()
+
+# Chamado quando inimigo colidir com Player
+func _on_body_entered(body: Node2D) -> void:
+	# Esconde playr
+	hide()
+	# Emite signal Hit (Para avisar o main)
+	hit.emit()
+	# Desativa colisor
+	$CollisionShape2D.set_deferred("disabled", true)
